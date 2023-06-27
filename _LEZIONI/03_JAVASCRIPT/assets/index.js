d2f1fdb8f551a0876af88748ac73ae250f7c2d88
Filippo <filippo.soff05@gmail.com>
2023-06-27 14:32:38 +0200

    js

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -1,6 +1,26 @@
-// Questo è un commento in JavaScript riga singola
-/* 
-    Questo è un commento in JavaScript multi riga 
-*/
+/*
 
-console.log("Hello World");
+Esercizio 1
+
+1) Creare una costante che contiene una lista di strighe
+2) Intercettare la div che contiene la lista dei box
+3) Creare un ciclo for che cicla la lista di stringhe
+4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
+
+ */
+
+
+const stringhe = 
+
+[
+    "Uno","Due","Tre","Quattro","Cinque","Sei","Sette","Otto","Nove"
+];
+
+const box = document.getElementsByClassName("box-list")[0];
+
+stringhe.map((item) => {
+
+    box.innerHTML += `<div class="box"><span>${item}</span></div>`
+
+});
+
