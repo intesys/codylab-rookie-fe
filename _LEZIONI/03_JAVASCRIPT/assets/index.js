d1c82892c7fd1952f969f603c55ced9a84329418
Richmond Zoogah <richiezoogah@yahoo.com>
2023-06-27 14:43:11 +0200

    Javascript

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -3,4 +3,43 @@
     Questo è un commento in JavaScript multi riga 
 */
 
-console.log("Hello World");
+
+/*
+
+Esercizio 1
+
+
+
+
+1) Creare una costante che contiene una lista di strighe
+
+2) Intercettare la div che contiene la lista dei box
+
+3) Creare un ciclo for che cicla la lista di stringhe
+
+4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
+
+
+*/
+
+
+
+
+ const lista = ["Richmond","Dalton","Solomon"];
+
+
+
+ 
+
+ const box = document.getElementsByClassName("box-list")[0];
+  
+ lista.map((item)=>{
+box.innerHTML += `<div class="box"><span>${item}</span></div>`
+ })
+
+
+
+ 
+ 
+
+ 
