b927f55abae30f42b7e12ef00001964a78fa81bd
unknown <matteo.ocneanu@gmail.com>
2023-06-27 14:40:59 +0200

    prova

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -3,4 +3,18 @@
     Questo è un commento in JavaScript multi riga 
 */
 
-console.log("Hello World");
+//creare una costante che contiene
+
+const stringhe = ["Totti", "Modric", "Ronaldo", "Son", "Messi", "Lukaku"];
+const boxList = document.getElementsByClassName("box-list")[0];
+for (let i=0; i<stringhe.length; i++){
+    const stringa = stringhe[i];
+    const box = document.createElement("div");
+    box.textContent = stringa;
+    i%2==0
+        ?box.style.backgroundColor="red"
+        :box.style.backgroundColor="#ffd1dc";
+    
+    box.classList.add("box");
+    boxList.appendChild(box);
+}
