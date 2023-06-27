29a7dea31d52d556557437a491e73443371ec679
Pietro Saccomando <pietro.saccomando@gmail.com>
2023-06-27 14:15:52 +0200

    index.js

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -1,6 +1,14 @@
-// Questo è un commento in JavaScript riga singola
-/* 
-    Questo è un commento in JavaScript multi riga 
-*/
+const stringhe = ["Stringa 1", "Stringa 2", "Stringa 3", "Stringa 4", "Stringa 5", "Stringa 6"];
+
+const boxList = document.getElementsByClassName("box-list")[0];
+
+for (let i=0; i<stringhe.length; i++){
+    const stringa = stringhe[i];
+    const box = document.createElement("div");
+    box.textContent = stringa;
+    box.classList.add("box");
+    boxList.appendChild(box);
+}
+
 
 console.log("Hello World");
