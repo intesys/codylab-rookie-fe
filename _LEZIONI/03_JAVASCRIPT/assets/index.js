0b6d05300702a5178fbb518f043ef67b858bda16
aleba <19285@studenti.marconiverona.edu.it>
2023-06-27 14:17:36 +0200

    esercizio javascript

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -3,4 +3,10 @@
     Questo è un commento in JavaScript multi riga 
 */
 
-console.log("Hello World");
+const pippobaudo = ["1","2","3","4","5","6"];
+
+const boxList = document.getElementsByClassName("box-list")[0];
+
+for (let i = 0; i < pippobaudo.length; i++) {
+    boxList.innerHTML += `<div class="box" style="background-color: green;"><span> ${pippobaudo[i]} </span></div>`;
+}
