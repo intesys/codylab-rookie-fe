16dd21e51cfb41afbded226a5f850fb2a5269c08
Filippo Dolci <filippo.dolci@intesys.it>
2023-06-27 14:16:35 +0200

    Fix: Aggiunta esercizio JS

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -2,5 +2,15 @@
 /* 
     Questo è un commento in JavaScript multi riga 
 */
+/* 
+
+Esercizio 1
+
+1) Creare una costante che contiene una lista di strighe
+2) Intercettare la div che contiene la lista dei box
+3) Creare un ciclo for che cicla la lista di stringhe
+4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
+
+*/
 
 console.log("Hello World");
