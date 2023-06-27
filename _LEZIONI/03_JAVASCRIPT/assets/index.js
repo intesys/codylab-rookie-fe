08ead0f2136cf106353dfba6c89157bf6919b91c
Kristian <19684@studenti.marconiverona.edu.it>
2023-06-27 14:15:26 +0200

    27/06/2023: javascript

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -1,6 +1,30 @@
-// Questo è un commento in JavaScript riga singola
-/* 
-    Questo è un commento in JavaScript multi riga 
+/*
+
+Esercizio 1
+
+
+ 
+
+1) Creare una costante che contiene una lista di strighe
+
+2) Intercettare la div che contiene la lista dei box
+
+3) Creare un ciclo for che cicla la lista di stringhe
+
+4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
+
+ 
+
 */
 
-console.log("Hello World");
+const stringhe = ["jala brat", "relja popovic", "vlada matovic", "djordje carkic", "rie", "buba coreli"];
+
+const boxList = document.getElementsByClassName("box-list")[0];
+
+for (let i=0; i<stringhe.length; i++){
+    const stringa = stringhe[i];
+    const box = document.createElement("div");
+    box.textContent = stringa;
+    box.classList.add("box");
+    boxList.appendChild(box);
+}
