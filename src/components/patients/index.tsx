2beadd7910423da6827b14f2a466c5a5aaf28b80
Pietro Saccomando <pietro.saccomando@gmail.com>
2023-06-30 17:40:01 +0200

    Lista dei pazienti, PatientBox

--- src/components/patients/index.tsx
+++ src/components/patients/index.tsx
@@ -1,35 +1,75 @@
-import React, { Dispatch, useMemo, useReducer } from "react";
+import { Grid } from "@mui/material";
+import React, { Dispatch, useMemo, useReducer, useState } from "react";
 import { PatientDTO, PatientFilterDTO } from "../../generated/axios";
 import Breadcrumb from "../Breadcrumb/Breadcrumb";
 import BreadcrumbEl from "../Breadcrumb/BreadcrumbEl";
+import PatientBox from "./PatientBox/index";
 import { Action, patientsFilterReducer } from "./lib";
 
-const PatientsList: PatientDTO[] = [
+const patientsList: PatientDTO[] = [
   {
     id: 1,
-    name: "Pietro",
-    surname: "Saccomando",
+    opd: 2334,
+    idp: 64464,
+    name: "Lionel",
+    surname: "Messi",
   },
 
   {
     id: 2,
-    name: "Aldo",
-    surname: "Maira",
+    opd: 2334,
+    idp: 64364,
+    name: "Nicolò",
+    surname: "Barella",
   },
   {
     id: 3,
-    name: "Pierdavide",
-    surname: "Roccaro",
+    opd: 2334,
+    idp: 64364,
+    name: "Cristiano",
+    surname: "Ronaldo",
   },
   {
     id: 4,
-    name: "Stefano",
-    surname: "Sanfilippo",
+    opd: 2334,
+    idp: 64364,
+    name: "Kylian",
+    surname: "Mbappè",
   },
   {
     id: 5,
-    name: "Filippo",
-    surname: "Dolci",
+    opd: 2334,
+    idp: 64364,
+    name: "Lautaro",
+    surname: "Martinez",
+  },
+  {
+    id: 6,
+    opd: 2334,
+    idp: 64364,
+    name: "Paulo",
+    surname: "Dybala",
+  },
+  {
+    id: 7,
+    opd: 2334,
+    idp: 64364,
+    name: "Romelu",
+    surname: "Lukaku",
+  },
+  {
+    id: 8,
+    opd: 2334,
+    idp: 64364,
+    name: "Federico",
+    surname: "Dimarco",
+  },
+  {
+    id: 9,
+    opd: 2334,
+    idp: 64364,
+    name: "Davide",
+    surname: "Frattesi",
   },
 ];
 
@@ -46,14 +86,24 @@ export const PatientsFilterContext: React.Context<IPatientsFilterContext> = Reac
 const Patients: React.FC = () => {
   const [filter, dispatch] = useReducer(patientsFilterReducer, {});
   const patientContextValue = useMemo(() => ({ filter, dispatch }), [filter, dispatch]);
-
+  const [loading, setLoading] = useState(false);
   return (
     <PatientsFilterContext.Provider value={patientContextValue}>
       <Breadcrumb>
         <BreadcrumbEl active>Patients</BreadcrumbEl>
       </Breadcrumb>
-      {/* Patients filter form */}
-      {/* Patients list */}
+
+      <Grid container mt={4} spacing={2}>
+        {loading ? (
+          <>Loading</>
+        ) : (
+          patientsList.map((patient) => (
+            <Grid item key={patient.id} xs={4}>
+              <PatientBox props={{ patient }} />
+            </Grid>
+          ))
+        )}
+      </Grid>
     </PatientsFilterContext.Provider>
   );
 };
