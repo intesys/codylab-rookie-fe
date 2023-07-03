133a71c7533ad5a1f9afdb601e4103eb460c8c94
Filippo <filippo.soff05@gmail.com>
2023-07-03 12:34:13 +0200

    Inizio Progetto

--- src/components/Doctors/index.tsx
+++ src/components/Doctors/index.tsx
@@ -1,32 +1,83 @@
-import React, { Dispatch, useMemo, useReducer } from "react";
-import { DoctorFilterDTO } from "../../generated/axios";
+import { Grid } from "@mui/material";
+import React, { Dispatch, Reducer } from "react";
+import { DoctorDTO, PatientFilterDTO } from "../../generated/axios";
 import Breadcrumb from "../Breadcrumb/Breadcrumb";
 import BreadcrumbEl from "../Breadcrumb/BreadcrumbEl";
-import { Action, doctorsFilterReducer } from "./lib";
+import PatientBox from "../Patients/PatientBox";
+import { patientsFilterReducer } from "../Patients/lib";
+import { Action } from "./lib";
 
-interface IDoctorsFilterContext {
-  filter: DoctorFilterDTO;
+const patientsList: DoctorDTO[] = [
+  {
+    id: 1,
+    name: "Marco",
+    surname: "Pietruzzo",
+  },
+  {
+    id: 2,
+    name: "Giovanni",
+    surname: "Ocneanu",
+  },
+  {
+    id: 3,
+    name: "Maria",
+    surname: "Antonietta",
+  },
+
+  {
+    id: 4,
+    name: "Jhon",
+    surname: "Cena",
+  },
+];
+
+interface IPatientsFilterContext {
+  filter: PatientFilterDTO;
   dispatch: Dispatch<Action>;
 }
 
-export const DoctorsFilterContext: React.Context<IDoctorsFilterContext> = React.createContext({
+export const PatientsFilterContext: React.Context<IPatientsFilterContext> = React.createContext({
   filter: {},
   dispatch: (action) => {},
 });
 
-const Doctors: React.FC = () => {
-  const [filter, dispatch] = useReducer(doctorsFilterReducer, {});
-  const doctorsContextValue = useMemo(() => ({ filter, dispatch }), [filter, dispatch]);
-
+const Patients: React.FC = () => {
+  const [filter, dispatch] = useReducer(patientsFilterReducer, {});
+  const patientContextValue = useMemo(() => ({ filter, dispatch }), [filter, dispatch]);
+  const [loading, setLoading] = useState(false);
   return (
-    <DoctorsFilterContext.Provider value={doctorsContextValue}>
+    <PatientsFilterContext.Provider value={patientContextValue}>
       <Breadcrumb>
-        <BreadcrumbEl active>Doctors</BreadcrumbEl>
+        <BreadcrumbEl active>Patients</BreadcrumbEl>
       </Breadcrumb>
-      {/* Doctors filter form */}
-      {/* Doctors list */}
-    </DoctorsFilterContext.Provider>
+
+      <Grid container mt={4} spacing={2}>
+        {loading ? (
+          <>Loading</>
+        ) : (
+          patientsList.map((patient) => (
+            <Grid item key={patient.id} xs={4}>
+              <PatientBox props={{ patient }} />
+            </Grid>
+          ))
+        )}
+      </Grid>
+    </PatientsFilterContext.Provider>
   );
 };
 
-export default Doctors;
+export default Patients;
+function useReducer(
+  patientsFilterReducer: Reducer<PatientFilterDTO, import("../Patients/lib").Action>,
+  arg1: {}
+): [any, any] {
+  throw new Error("Function not implemented.");
+}
+
+function useMemo(arg0: () => { filter: any; dispatch: any }, arg1: any[]) {
+  throw new Error("Function not implemented.");
+}
+
+function useState(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.");
+}
