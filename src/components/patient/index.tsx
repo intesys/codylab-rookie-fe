c2f953839c65c48de8f00dd334a4ff05b60f60d4
Denis Gandibula <119001838+DenisGandibula@users.noreply.github.com>
2024-07-05 10:16:42 +0200

    Add files via upload

--- src/components/patient/index.tsx
+++ src/components/patient/index.tsx
@@ -1,17 +1,19 @@
 import Breadcrumb from "@components/Breadcrumb/Breadcrumb";
 import BreadcrumbEl from "@components/Breadcrumb/BreadcrumbEl";
 import { api } from "@config/api";
-import { PATIENTS_PATH, PATIENTS_RECORDS_PATH } from "@config/paths";
+import { DOCTORS_PATH, PATIENTS_PATH, PATIENTS_RECORDS_PATH } from "@config/paths";
 import { DetailType } from "@lib/types";
-import { generateAvatarImage, getBloodType, getEditDetailPath, getPath } from "@lib/utils";
-import { Avatar, Box, Button, Grid } from "@mui/material";
+import { generateAvatarImage, getBloodType, getDetailPath, getEditDetailPath, getPath } from "@lib/utils";
+import { Avatar, Box, Button, CircularProgress, Grid } from "@mui/material";
 import { red } from "@mui/material/colors";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
 import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
 
@@ -19,14 +21,17 @@ import "./index.scss";
 
 import { DATE_FORMAT } from "@config/date";
 import { PatientDTO } from "@generated/axios";
+import useGetDetail from "@hooks/useGetDetail";
 import DeleteIcon from "@mui/icons-material/Delete";
 import dayjs from "dayjs";
 import { enqueueSnackbar } from "notistack";
 
+const newRecord = {} as PatientDTO;
+
 const Patient: React.FC = () => {
-  const [patient, setPatient] = useState<PatientDTO>();
-  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+
+  const [patient, loading, getDetail] = useGetDetail(api.patients.getPatient, newRecord, Number(id));
 
   const navigate = useNavigate();
 
@@ -39,8 +44,15 @@ const Patient: React.FC = () => {
   }, [id]);
 
   const handlDeleteRecordClick = (recordId: number) => {
-    api.patientRecords.deletePatientRecord(recordId);
-    window.location.reload();
+    api.patientRecords
+      .deletePatientRecord(recordId)
+      .then(() => {
+        getDetail();
+        enqueueSnackbar(`Success: Record ELIMINATO`, { variant: "success" });
+      })
+      .catch(() => {
+        enqueueSnackbar(`error`, { variant: "error" });
+      });
   };
 
   const handlAddRecordClick = useCallback(() => {
@@ -51,18 +63,13 @@ const Patient: React.FC = () => {
     navigate(getEditDetailPath(PATIENTS_PATH, id));
   }, [navigate]);
 
-  useEffect(() => {
-    if (!loading) {
-      return;
-    }
-    api.patients.getPatient(Number(id)).then((response) => {
-      setPatient(response.data);
-    });
-    if (!patient) {
-      return;
-    }
-    setLoading(false);
-  }, [loading, id]);
+  const handleDoctorClick = (id: number) => {
+    navigate(getDetailPath(DOCTORS_PATH, id));
+  };
+
+  if (loading) {
+    <CircularProgress />;
+  }
 
   if (!patient) {
     return <>Patient not found</>;
@@ -101,6 +108,9 @@ const Patient: React.FC = () => {
               sx={{ color: red[500] }}
               fontSize="small"
               onClick={() => handlEditPatientClick()}
+              style={{
+                cursor: "pointer",
+              }}
             ></ModeEditOutlineIcon>
           </p>
           <p className="patient-address">{patient.address}</p>
@@ -159,7 +169,13 @@ const Patient: React.FC = () => {
 
           <h4>LAST DOCTOR WHO VISIT THE PATIENT</h4>
           {patient.patientRecords && patient.patientRecords.length > 0 && patient.patientRecords[0].doctor && (
-            <div className="box-last-patient">
+            <div
+              className="box-last-patient"
+              style={{
+                cursor: "pointer",
+              }}
+              onClick={() => handleDoctorClick(Number(patient.patientRecords[0].doctor?.id))}
+            >
               <div>
                 <Avatar
                   alt="Remy Sharp"
@@ -172,6 +188,19 @@ const Patient: React.FC = () => {
               </p>
             </div>
           )}
+
+          <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+            <LocalPhoneIcon className="doctor-details-phone-number-icon" sx={{ color: red[500] }}></LocalPhoneIcon>
+            {patient.patientRecords && patient.patientRecords.length > 0 && (
+              <p className="doctor-details-phone-number-text">{patient.patientRecords[0].doctor?.phoneNumber}</p>
+            )}
+          </Grid>
+          <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+            <MailOutlineIcon className="doctor-details-email-icon" sx={{ color: red[500] }}></MailOutlineIcon>
+            {patient.patientRecords && patient.patientRecords.length > 0 && (
+              <p className="doctor-details-email-text">{patient.patientRecords[0].doctor?.email}</p>
+            )}
+          </Grid>
         </div>
 
         {/* Record Informations*/}
@@ -253,6 +282,9 @@ const Patient: React.FC = () => {
                     <DeleteOutlineIcon
                       fontSize="large"
                       onClick={() => handlDeleteRecordClick(Number(record.id))}
+                      style={{
+                        cursor: "pointer",
+                      }}
                     ></DeleteOutlineIcon>
                   </h4>
                 </Grid>
