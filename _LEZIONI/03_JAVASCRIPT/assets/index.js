0f81c7ed9c23196c495155c85925efdb2bd04a81
unknown <itsmordiagne@gmail.com>
2025-06-23 15:58:22 +0200

    Esercizi

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -1,16 +1,49 @@
-// Questo è un commento in JavaScript riga singola
-/* 
-    Questo è un commento in JavaScript multi riga 
-*/
-/* 
+/*Esercizio 1
+let nome = "Mor";
+let eta = 18; 
+console.log(nome);
+console.log(eta);*/
 
-Esercizio 1
+/*Esercizio 2
+let numero = 10;
+let numero1 = 8;
+let somma = numero + numero1;
+console.log("La somma è:", somma);    
+let prodotto = numero * numero1;         
+console.log("il prodotto è:", prodotto);*/
 
-1) Creare una costante che contiene una lista di strighe
-2) Intercettare la div che contiene la lista dei box
-3) Creare un ciclo for che cicla la lista di stringhe
-4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
+/*Esercizio 3
+let frutti = ["Pera", "Mango", "Mela"];  
+console.log("il primo frutto è:", frutti[0]);             
+console.log("l'Ultimo frutto è:", frutti[frutti.length - 1]);*/
 
-*/
+/*Esercizio 4
+let frutti = ["Pera", "Mango", "Mela"]; 
+frutti.push("Banana");
+console.log(frutti);  
+frutti.shift();
+console.log(frutti);*/
 
-console.log("Hello World");
+/*Esercizio 5 
+let studente = {
+  nome: "Mor",
+  cognome: "Diagne",
+  eta: 18
+};
+console.log("Il nome completo è:", studente.nome + " " + studente.cognome);*/
+
+/*Esercizio 6
+interface Product {
+  name: string;
+  price: number;
+  inStock: boolean;
+}
+function getInStock(products: Product[]) {
+  return products.filter(p => p.inStock);
+}
+const items = [
+  { name: "Pera", price: 1, inStock: true },
+  { name: "Mango", price: 2, inStock: false },
+  { name: "Banana", price: 1.5, inStock: true }
+];
+console.log(getInStock(items));*/
