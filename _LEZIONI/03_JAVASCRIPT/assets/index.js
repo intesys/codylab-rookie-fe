6ec8098ce66702fba0664e48e376fbcc2dd719ca
unknown <damipisci434@gmail.com>
2025-06-23 15:55:51 +0200

    esercizi js

--- _LEZIONI/03_JAVASCRIPT/assets/index.js
+++ _LEZIONI/03_JAVASCRIPT/assets/index.js
@@ -1,16 +1,48 @@
-// Questo è un commento in JavaScript riga singola
-/* 
-    Questo è un commento in JavaScript multi riga 
+/*es 1
+let nome = "Damiano"
+let eta = 18
+console.log(nome, eta)*/
+
+/*es 2
+let x = 15
+let y = 2
+console.log(x+y, x*y)*/
+
+/*es 3
+let frutti = ["Mela", "Ananas", "Banana"]
+console.log(frutti[0], frutti[2])*/
+
+/*es 4
+let frutti = ["Mela", "Ananas", "Banana"]
+frutti.push("Ciliegie")
+console.log(frutti)
+frutti.shift()
+console.log(frutti)*/
+
+/*es 5
+const studente = {
+    nome: "Damiano",
+    cognome: "Piscitelli",
+    eta: 18
+}
+console.log(studente.nome, studente.cognome)*/
+
+/*es 6
+interface Product{
+    name: String;
+    price: Number;
+    inStock: Boolean;
+}
+
+function getAvailableProducts(products: Product[]): Product[] {
+  return products.filter(product => product.inStock);
+}
+const inventory: Product[] = [
+  { name: "Laptop", price: 1200, inStock: true },
+  { name: "Mouse", price: 25, inStock: false },
+  { name: "Keyboard", price: 75, inStock: true },
+];
+
+const available = getAvailableProducts(inventory);
+console.log(available);
 */
-/* 
-
-Esercizio 1
-
-1) Creare una costante che contiene una lista di strighe
-2) Intercettare la div che contiene la lista dei box
-3) Creare un ciclo for che cicla la lista di stringhe
-4) Per ogni stringa creare un box con il contenuto della stringa appendendolo alla div che contiente la lista dei box
-
-*/
-
-console.log("Hello World");
